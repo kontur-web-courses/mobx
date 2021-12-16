f71790e8b9388faad24a883832f7afea45c6d502
nulladdict <nulladdicted@gmail.com>
2021-12-16 20:15:42 +0500

    build: update deps

--- tasks/snowpack.config.js
+++ tasks/snowpack.config.js
@@ -1,4 +1,4 @@
-/** @type {import("snowpack").SnowpackUserConfig } */
+/** @type {import("snowpack").SnowpackUserConfig} */
 module.exports = {
   mount: {
     public: { url: "/", static: true },
@@ -9,21 +9,4 @@ module.exports = {
     "@snowpack/plugin-dotenv",
     "@snowpack/plugin-typescript",
   ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
 };
