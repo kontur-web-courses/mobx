6efd063ed4e483f3a0376b414edc2db2a6a443b7
nulladdict <nulladdicted@gmail.com>
2021-12-16 20:16:24 +0500

    feat(tasks): add api solution

--- tasks/src/1.api/api.ts
+++ tasks/src/1.api/api.ts
@@ -1,4 +1,4 @@
-import { makeObservable, autorun } from "mobx";
+import { makeObservable, autorun, observable, action, computed } from "mobx";
 
 enum Operator {
   Addition = "+",
@@ -13,26 +13,42 @@ class Calculator {
   right = NaN;
 
   constructor() {
-    makeObservable(this, {});
+    makeObservable(this, {
+      left: observable,
+      operator: observable,
+      right: observable,
+      input: action,
+      result: computed,
+    });
   }
 
-  static compute(lhs: number, operator: Operator, rhs: number) {
-    console.log(`computing: ${lhs} ${operator} ${rhs}`);
-    return eval(`${lhs} ${operator} ${rhs}`);
+  input(lhs: number, operator: Operator, rhs: number) {
+    this.left = lhs;
+    this.operator = operator;
+    this.right = rhs;
+  }
+
+  get result() {
+    const expression = `${this.left} ${this.operator} ${this.right}`;
+    console.log(`computing: ${expression}`);
+    return eval(expression);
   }
 }
 
 const calc = new Calculator();
 
 autorun(() => {
-  const result = Calculator.compute(calc.left, calc.operator, calc.right);
-  console.log(`result: ${result}`);
+  console.log(`result: ${calc.result}`);
 });
 
 // Посчитай 2 + 2
+calc.input(2, Operator.Addition, 2);
 
 // Посчитай 2 * 2
+calc.input(2, Operator.Multiplication, 2);
 
 // Посчитай 2 - 2
+calc.input(2, Operator.Subtraction, 2);
 
 // Посчитай 1 / 3
+calc.input(1, Operator.Division, 3);
