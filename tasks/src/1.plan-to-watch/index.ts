959e7384a9bf96f929d29bf8ec74605b69198bc4
nulladdict <nulladdicted@gmail.com>
2021-12-16 20:15:44 +0500

    refactor(tasks): move things around

--- tasks/src/1.plan-to-watch/index.ts
+++ tasks/src/1.plan-to-watch/index.ts
@@ -4,7 +4,6 @@ import { PlanToWatchNavigation } from "./components/Navigation";
 import { PlanToWatchEntry } from "./components/Entry";
 import { PlanToWatchList } from "./components/List";
 import { defaultList, ListStore } from "./models/list";
-import { apiClient } from "./api/client";
 
 function whenDefined() {
   return Promise.all([
@@ -18,8 +17,6 @@ function whenDefined() {
 export async function render(rootElement: HTMLElement) {
   await whenDefined();
   const ptwList = document.createElement(PlanToWatchList.is) as PlanToWatchList;
-  console.log(apiClient);
-  // TODO: Запрашивай это из апи
-  ptwList.list = new ListStore(await apiClient.getList());
+  ptwList.list = new ListStore(defaultList);
   rootElement.appendChild(ptwList);
 }
